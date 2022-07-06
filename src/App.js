cc324ae347ee0d2ac6aec405974a1ac4ebea9db4
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-07-06 21:51:07 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import axios from 'axios';
+
+import { Link } from 'react-router-dom';
 import './App.css';
+import WelcomePage from './pages/welcomePage/WelcomePage';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <WelcomePage /> */}
+      <nav
+        style={{
+          borderBottom: 'solid 1px',
+          paddingBottom: '1rem',
+        }}
+      >
+        <Link to="/WelcomePage">welcomePage</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
+//  <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<WelcomePage />}>
+//           <Route index element={<Home />} />
+//           <Route path="blogs" element={<Blogs />} />
+//           <Route path="contact" element={<Contact />} />
+//           <Route path="*" element={<NoPage />} />
+//         </Route>
+//       </Routes>
+//     </BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import WelcomePage from './pages/welcomePage/WelcomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
-    <App />
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/welcomePage" element={<WelcomePage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
