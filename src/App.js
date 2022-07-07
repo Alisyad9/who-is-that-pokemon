682ef7484092f5b557c8bccc196fdff4042149d2
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-07-07 20:09:11 +0100

    deleted unused components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import './App.css';
 import WelcomePage from './pages/welcomePage/WelcomePage';
 
 function App() {
+  let num1 = 1;
+  let num2 = 1;
+  console.log(num2 === num1);
+
+  function name(randomNumber1, randomNumber2) {
+    if (randomNumber1 === randomNumber2) {
+      console.log('true');
+    }
+  }
+  name(num2, num1);
   return <div className="App">hey</div>;
 }
 
