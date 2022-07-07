2b63bb8f2f475a4865ec08023cb34feb6008f439
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-07-07 13:52:12 +0100

    index.js is now our main router for the pages
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import Layout from './pages/layout/Layout';
 import WelcomePage from './pages/welcomePage/WelcomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<App />} />
         <Route path="/welcomePage" element={<WelcomePage />} />
       </Route>
     </Routes>
