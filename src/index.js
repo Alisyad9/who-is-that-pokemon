ea6b4bd6aed1c95ad1eadf0c000a1a97382525b7
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-07-07 20:09:48 +0100

    linked the notFound page to the router
    Co-authored-by: Adam <31065348+adam8-9@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
+import NotFound from './pages/Incorrect/NotFound';
 import Layout from './pages/layout/Layout';
 import WelcomePage from './pages/welcomePage/WelcomePage';
 
@@ -13,6 +14,16 @@ root.render(
         <Route index element={<App />} />
         <Route path="/welcomePage" element={<WelcomePage />} />
       </Route>
+      <Route path="incorrect" element={<NotFound />} />
     </Routes>
   </BrowserRouter>
 );
+
+{
+  /* <Route path="/" element={<App />}>
+<Route path="foo" element={<Foo />} />
+<Route path="bar" element={<Bar />} />
+<Route path="baz" element={<Baz />} />
+<Route path="blah" element={<Blah />} />
+</Route> */
+}
