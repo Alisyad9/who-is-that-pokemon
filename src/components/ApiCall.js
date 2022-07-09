6de31bafde9bc3b77e2a7d5f704925ee95fe1bb4
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-07-09 20:48:17 +0100

    able to randomise the buttons,,,

--- src/components/ApiCall.js
+++ src/components/ApiCall.js
@@ -5,25 +5,52 @@ import { Link } from 'react-router-dom';
 
 const ApiCall = () => {
   const [pokemonImage, setPokemonImage] = useState('');
-  const [fakeName, setFakename] = useState('');
+  const [fakeName1, setFakename1] = useState('');
+  const [fakeName2, setFakename2] = useState('');
   const [realName, setRealName] = useState('');
   const [isClicked, setIsClicked] = useState(false);
+  ////////////////////////////////
 
-  const [rightButton, setRightButton] = useState('');
+  ////////////////////////////////
+  let asyncResult;
+  (async function () {
+    const asyncFunctions = [fakePokemon1, fakePokemon2, onSubmitHandler];
+    const results = await Promise.all(asyncFunctions);
+    // outputs `[2, 3, 5]` after five seconds
+    console.log(results);
+    asyncResult = results;
+    console.log(asyncResult);
+  })();
 
-  console.log('isClicked value', isClicked);
+  // const randomName = Math.floor(Math.random() * array.length);
+  // console.log('array result', array);
+  // console.log('Random array name', array[randomName);
 
   const randomNumber1 = Math.floor(Math.random() * (50 - 1) + 1);
   const randomNumber2 = Math.floor(Math.random() * (100 - 51) + 51);
+  const randomNumber3 = Math.floor(Math.random() * (150 - 101) + 101);
 
-  const fakePokemon = async () => {
+  const fakePokemon1 = async () => {
     try {
       await axios({
         method: 'get',
         url: `https://pokeapi.co/api/v2/pokemon/${randomNumber2}/`,
       }).then((res) => {
         console.log(res.data);
-        setFakename(res.data.name);
+        setFakename1(res.data.name);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fakePokemon2 = async () => {
+    try {
+      await axios({
+        method: 'get',
+        url: `https://pokeapi.co/api/v2/pokemon/${randomNumber3}/`,
+      }).then((res) => {
+        console.log(res.data);
+        setFakename2(res.data.name);
       });
     } catch (error) {
       console.log(error);
@@ -45,11 +72,33 @@ const ApiCall = () => {
     } catch (error) {
       console.log(error);
     }
-    fakePokemon();
+    fakePokemon1();
+    fakePokemon2();
   };
 
   ////////
+  let array = [fakeName1, fakeName2, realName];
+  console.log(array);
 
+  let shuffled = array
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
+  console.log(shuffled.map((value) => value === realName));
+
+  function arraySwap() {
+    return (
+      <div>
+        {shuffled.map((value) => (
+          <ul>
+            <li key={value}>
+              <button>{value}</button>
+            </li>
+          </ul>
+        ))}
+      </div>
+    );
+  }
   return (
     <div>
       <div className="border">
@@ -58,15 +107,31 @@ const ApiCall = () => {
         </div>
       </div>
 
+      {isClicked
+        ? shuffled.map((value) => (
+            <ul>
+              <li key={value}>
+                {value === realName ? (
+                  <button onClick={onSubmitHandler}>{value}</button>
+                ) : (
+                  <Link to="/incorrect">
+                    <button>{value}</button>
+                  </Link>
+                )}
+              </li>
+            </ul>
+          ))
+        : ''}
+
       {isClicked ? (
         <div>
           <Link to="/incorrect">
-            <button value={rightButton}>{fakeName}</button>
+            <button>{fakeName1}</button>
+            <button>{fakeName2}</button>
           </Link>
 
-          <button onClick={onSubmitHandler} value={rightButton}>
-            {realName}
-          </button>
+          <button onClick={onSubmitHandler}>{realName}</button>
+          {/* <button onClick={onSubmitHandler}>{arraySwap()}</button> */}
         </div>
       ) : (
         <form onSubmit={onSubmitHandler}>
@@ -74,9 +139,30 @@ const ApiCall = () => {
         </form>
       )}
 
-      {rightButton && <div>Well done</div>}
+      {/* {shuffled.map((value) => (
+        <ul>
+          <li key={value}>
+            <button>{value}</button>
+          </li>
+        </ul>
+      ))} */}
+      <div>{arraySwap()}</div>
     </div>
   );
 };
 
 export default ApiCall;
+
+// li {
+//   border: 10px;
+//   color: red;
+//   margin: 5px;
+// }
+
+// ul.demo {
+//   list-style-type: none;
+//   border: 2px;
+//   margin: 3px;
+//   display: flex;
+//   flex-direction: row;
+// }
