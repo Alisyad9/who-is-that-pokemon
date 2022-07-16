0fd174c089bbfb78d8a7890bd5fa1f3a82f43237
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-07-16 22:46:13 +0100

    added the number counter
    it now displays how many times you guessed correct
    
    Co-Authored-By: Adam <31065348+adam8-9@users.noreply.github.com>

--- src/components/ApiCall.js
+++ src/components/ApiCall.js
@@ -1,3 +1,6 @@
+import dog from './dog.jpg';
+import unknown from './unknown.png';
+
 import { Button } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
@@ -8,22 +11,27 @@ import {
   Image,
   List,
   FormControl,
+  Text,
+  Fade,
+  useDisclosure,
 } from '@chakra-ui/react';
 
 import { Link } from 'react-router-dom';
 
 const ApiCall = () => {
+  const { isOpen, onToggle } = useDisclosure();
   const [pokemonImage, setPokemonImage] = useState('');
   const [fakeName1, setFakename1] = useState('');
   const [fakeName2, setFakename2] = useState('');
   const [realName, setRealName] = useState('');
   const [isClicked, setIsClicked] = useState(false);
   const [counter, setCounter] = useState(0);
+  const [counter1, setCounter1] = useState(0);
 
   ////////////////////////////////////////////////////////////////
 
   const property = {
-    imageUrl: 'https://bit.ly/2Z4KKcF',
+    imageUrl: 'https://unsplash.com/photos/Mv9hjnEUHR4',
     imageAlt: 'Rear view of modern home with pool',
     beds: 3,
     baths: 2,
@@ -86,8 +94,10 @@ const ApiCall = () => {
         setPokemonImage(res.data.sprites.front_default);
         setRealName(res.data.name);
         setIsClicked(true);
-
         setCounter((count) => count + 1);
+        if (counter >= 1) {
+          setCounter1((old) => old + 1);
+        }
       });
     } catch (error) {
       console.log(error);
@@ -95,6 +105,8 @@ const ApiCall = () => {
     fakePokemon1();
     fakePokemon2();
   };
+
+  const incrementHandler = () => {};
 
   ////////
   let array = [fakeName1, fakeName2, realName];
@@ -115,16 +127,16 @@ const ApiCall = () => {
     >
       <div>
         {/* <img src={pokemonImage}></img> */}
-        <Box maxW="sm" borderWidth="3px" borderRadius="lg" overflow="hidden">
+        <Box maxW="sm" boxShadow="dark-lg" p="1" rounded="md">
           {' '}
-          <Image src={pokemonImage} />
+          {!isClicked ? <Image src={unknown} /> : <Image src={pokemonImage} />}
         </Box>
       </div>
-      <Flex mt="1rem" flexDirection="row">
+      <Flex mt="0.8rem" flexDirection="row">
         {isClicked
           ? shuffled.map((value) => (
               <ul>
-                <List m="1rem" key={value}>
+                <List m="0.8rem" key={value}>
                   {value === realName ? (
                     <Button colorScheme="facebook" onClick={onSubmitHandler}>
                       {value}
@@ -139,15 +151,28 @@ const ApiCall = () => {
             ))
           : ''}
       </Flex>
-      {!isClicked && (
-        <FormControl>
-          <button></button>
-          <Button mt={4} colorScheme="teal" onClick={onSubmitHandler}>
-            New game
-          </Button>
-        </FormControl>
-      )}
-      <div>{!setIsClicked ? '' : counter}</div>
+      <Flex textAlign="center" mt="0.8rem">
+        {!isClicked && (
+          <FormControl>
+            <button></button>
+            <Button mt={4} colorScheme="teal" onClick={onSubmitHandler}>
+              New game
+            </Button>
+          </FormControl>
+        )}
+      </Flex>
+      <Flex textAlign="center" mt="0.8rem">
+        {' '}
+        <Text>
+          Your Score{' '}
+          <Text as="mark">
+            <Text fontSize="3xl" color="red">
+              {' '}
+              {counter1}
+            </Text>
+          </Text>
+        </Text>
+      </Flex>
     </Flex>
   );
 };
