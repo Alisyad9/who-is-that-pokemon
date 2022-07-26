c0ba9bd599e1a2846266cf5b15103af41036a017
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-07-26 21:47:42 +0100

    image working

--- src/pages/Incorrect/NotFound.js
+++ src/pages/Incorrect/NotFound.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import unknown from '../../components/unknown.png';
-
+import modern from './modern.png';
 import {
   Container,
   Flex,
@@ -18,9 +18,9 @@ const NotFound = () => {
     <Flex
       minH="100vh"
       justifyContent="center"
-      backgroundImage={unknown}
-      backgroundPosition="center"
+      backgroundImage={modern}
       backgroundRepeat="no-repeat"
+      backgroundSize="cover"
     >
       Wrong pokemon try again
     </Flex>
