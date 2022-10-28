cef3c972b43de295806d2473eaf1f65e9cce51cc
Alisyad9 <76549147+Alisyad9@users.noreply.github.com>
2022-10-28 11:08:00 +0100

    removed

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import WelcomePage from './pages/welcomePage/WelcomePage';
 import { Text } from '@chakra-ui/react';
 
 function App() {
-  return (
-    <Text textAlign="center" fontSize="50px" color="tomato">
-      I'm using a custom font-size value for this text
-    </Text>
-  );
+  return <Text textAlign="center" fontSize="50px" color="tomato"></Text>;
 }
 
 export default App;
